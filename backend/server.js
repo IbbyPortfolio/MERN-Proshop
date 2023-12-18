d6d22cf95d82eab87559e7e60dbcd97fd868839e
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2023-12-19 03:58:22 +1100

    Seed data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDb from "./config/db.js"
 import products from "./data/products.js";
 
+
 const port = process.env.PORT ||5000;
+
+connectDb()// Connect to MongoDb
 const app = express();
 
 app.get("/", (req, res) => {
